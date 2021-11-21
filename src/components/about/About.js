e3bf7abe28c2245abe35a8e22e418d7a50306c4d
pavan <pavanpk0850@gmail.com>
2021-11-21 17:26:09 +0530

    added github and twitter btn

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -14,6 +14,22 @@ const About = ({ isDark }) => {
 					Hello! My name is <span className='bg-gradient-to-r from-[#FA8142] to-[#FF4088] text-white rounded p-[1px] sm:p-[2px] :mx-[5px] whitespace-pre sm:whitespace-normal'>Pavan Kumar</span> - I absolutely love the process of creating things through
 					code. It involves a combination of some of my favorite things: logic, creativity, and computers! I genuinely have a passion for people and always enjoy meeting new people!
 				</p>
+				<div className='mt-10 mb-5 space-x-3 flex  w-full'>
+					<a
+						href='https://github.com/pavandot'
+						target='_blank'
+						rel='noreferrer'
+						className=' text-text-light dark:text-text-dark font-semibold px-[20px] py-[7px] border-[2px] rounded border-[#fa8142] hover:bg-[#fa8142] transition-colors duration-300 hover:text-[#fff] dark:hover:text-[#fff]'>
+						GitHub
+					</a>
+					<a
+						href='https://twitter.com/pavanreddy0850'
+						target='_blank'
+						rel='noreferrer'
+						className=' text-text-light dark:text-text-dark font-semibold px-[20px] py-[7px] border-[2px] rounded border-[#09c372] hover:bg-[#09c372] transition-colors duration-300 hover:text-[#fff] dark:hover:text-[#fff]'>
+						Twitter
+					</a>
+				</div>
 			</div>
 			<div className='md:w-[30%] mr-[50px]'>
 				<img src='https://pavandot.netlify.app/static/media/profile.275b4e06.jpg' alt='my img' width='300' className=' rounded-lg' />
