cf01d1deece2eaff9f08d224cc37118fc8beb096
pavan <pavandotnet007@outlook.com>
2022-06-04 11:01:24 +0530

    added new skills

--- src/components/skills/SingleSkillComp.js
+++ src/components/skills/SingleSkillComp.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const SingleSkillComp = ({ isDark, skill }) => {
 	const { title, img, icon } = skill;
@@ -6,8 +6,8 @@ const SingleSkillComp = ({ isDark, skill }) => {
 	return (
 		<div className='bg-nav-light dark:bg-nav-dark flex  flex-col justify-between rounded-md p-2 shadow-lg transition-colors duration-300'>
 			<div className='flex-grow self-center '>
-				{/* <img src={img} alt='javascript' className='w-full h-full ' /> */}
-				<i className={`${icon} colored  text-[5.5rem]`}></i>
+				{img && <img src={img} alt='javascript' className='w-[88px] h-[88px] ' />}
+				{icon && <i className={`${icon} colored  text-[5.5rem]`}></i>}
 			</div>
 			<div className=''>
 				<h1 className='text-text-bold text-center text-xl font-bold dark:text-[#fff] '>{title}</h1>
--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,63 +1,84 @@
-import React from "react";
-import { Teacher } from "iconsax-react";
-import SingleSkillComp from "./SingleSkillComp";
+import React from 'react';
+import { Teacher } from 'iconsax-react';
+import SingleSkillComp from './SingleSkillComp';
 const Skills = ({ isDark }) => {
-	const iconColorLight = "#12181b";
-	const iconColorDark = "#fff";
+	const iconColorLight = '#12181b';
+	const iconColorDark = '#fff';
 	const skills = [
 		{
 			id: 1,
-			title: "Javascript",
+			title: 'Javascript',
 			// img: "https://www.freepnglogos.com/uploads/javascript-png/javascript-vector-logo-yellow-png-transparent-javascript-vector-12.png",
-			icon: "devicon-javascript-plain",
-			description: "an object-oriented computer programming language commonly used to create interactive effects within web browsers.",
+			icon: 'devicon-javascript-plain',
+			description:
+				'an object-oriented computer programming language commonly used to create interactive effects within web browsers.',
 		},
 		{
 			id: 2,
-			title: "ReactJs",
+			title: 'ReactJs',
 			// img: "https://iconape.com/wp-content/files/tx/353401/png/react-logo.png",
-			icon: "devicon-react-original",
-			description: "React is a free and open-source front-end JavaScript library for building user interfaces or UI components.",
+			icon: 'devicon-react-original',
+			description: 'React is a free and open-source front-end JavaScript library for building user interfaces or UI components.',
 		},
 		{
 			id: 3,
-			title: "Redux",
+			title: 'Redux',
 			// img: "https://iconape.com/wp-content/files/te/353276/png/redux-logo.png",
-			icon: "devicon-redux-original",
-			description: "Redux is an open-source JavaScript library for managing and centralizing application state.",
+			icon: 'devicon-redux-original',
+			description: 'Redux is an open-source JavaScript library for managing and centralizing application state.',
 		},
 		{
 			id: 4,
-			title: "tailwindcss",
+			title: 'tailwindcss',
 			// img: "https://iconape.com/wp-content/files/pq/371539/svg/371539.svg",
-			icon: "devicon-tailwindcss-plain",
-			description: "Tailwind CSS is a utility-first CSS framework for rapidly building custom user interfaces",
+			icon: 'devicon-tailwindcss-plain',
+			description: 'Tailwind CSS is a utility-first CSS framework for rapidly building custom user interfaces',
 		},
 		{
 			id: 5,
-			title: "Git",
+			title: 'Git',
 			// img: "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
-			icon: "devicon-git-plain",
-			description: "Git is a free and open source distributed version control system .",
+			icon: 'devicon-git-plain',
+			description: 'Git is a free and open source distributed version control system .',
 		},
 		{
 			id: 6,
-			title: "Next.js",
+			title: 'Next.js',
 			// img: "https://seeklogo.com/images/N/next-js-logo-8FCFF51DD2-seeklogo.com.png",
-			icon: "devicon-nextjs-original",
+			icon: 'devicon-nextjs-original',
 		},
 		{
 			id: 7,
-			title: "TypeScript",
+			title: 'TypeScript',
 			// img: "https://seeklogo.com/images/N/next-js-logo-8FCFF51DD2-seeklogo.com.png",
-			icon: "devicon-typescript-plain",
+			icon: 'devicon-typescript-plain',
+		},
+		{
+			id: 8,
+			title: 'Cypress',
+			img: 'https://avatars.githubusercontent.com/u/8908513?s=200&v=4',
+		},
+		{
+			id: 9,
+			title: 'React Testing Library',
+			img: 'https://testing-library.com/img/octopus-128x128.png',
+		},
+		{
+			id: 10,
+			title: 'Jest',
+			icon: 'devicon-jest-plain',
 		},
 	];
 	return (
-		<section className='bg-background-light dark:bg-background-dark  scroll-mt-[4rem] px-5 py-10 transition-colors duration-300 lg:px-[100px]' id='skills'>
+		<section
+			className='bg-background-light dark:bg-background-dark  scroll-mt-[4rem] px-5 py-10 transition-colors duration-300 lg:px-[100px]'
+			id='skills'
+		>
 			<div className='mx-auto flex max-w-6xl cursor-pointer items-center '>
 				<Teacher size='27' color={isDark ? iconColorDark : iconColorLight} variant='Bulk' />
-				<span className='text-text-bold self-end pl-[2px] text-[1.5rem] font-bold transition-all duration-500 dark:text-[#fff]'>SKILLS</span>
+				<span className='text-text-bold self-end pl-[2px] text-[1.5rem] font-bold transition-all duration-500 dark:text-[#fff]'>
+					SKILLS
+				</span>
 			</div>
 			<div className='mx-auto grid max-w-6xl grid-cols-2 gap-2 pt-10 sm:grid-cols-4 sm:gap-5'>
 				{skills.map((skill) => {
